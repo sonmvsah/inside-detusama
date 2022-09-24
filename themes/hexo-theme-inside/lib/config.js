6cae8ee4167c946e91f979cd1f8e01e705fab2f2
sonmvsah <sonmv.sah@gmail.com>
2022-09-24 12:05:00 +0700

    update

--- themes/hexo-theme-inside/lib/config.js
+++ themes/hexo-theme-inside/lib/config.js
@@ -1,27 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const gravatar = require('hexo/lib/plugins/helper/gravatar');
-const { date } = require('hexo/lib/plugins/helper/date');
-const urlFor = require('hexo/lib/plugins/helper/url_for');
-const utils = require('./utils');
-const pkg = require('../package.json');
-const configSchema = require('./configSchema.json');
-const manifest = require('../source/_manifest.json');
+const fs = require("fs");
+const path = require("path");
+const gravatar = require("hexo/lib/plugins/helper/gravatar");
+const { date } = require("hexo/lib/plugins/helper/date");
+const urlFor = require("hexo/lib/plugins/helper/url_for");
+const utils = require("./utils");
+const pkg = require("../package.json");
+const configSchema = require("./configSchema.json");
+const manifest = require("../source/_manifest.json");
 const { css } = require(`../source/${manifest.theme}`);
 
 module.exports = function (hexo) {
-  const js = hexo.extend.helper.get('js').bind(hexo);
-  const pluginManifest = loadManifest([path.join(hexo.theme_dir, 'lib/plugins')]);
+  const js = hexo.extend.helper.get("js").bind(hexo);
+  const pluginManifest = loadManifest([
+    path.join(hexo.theme_dir, "lib/plugins"),
+  ]);
 
-  hexo.on('generateBefore', function () {
+  hexo.on("generateBefore", function () {
     const site = hexo.config;
     const theme = Object.assign(hexo.theme.config || {}, site.theme_config);
-    const email = (theme.profile && theme.profile.email) || site.email || '';
-    const feed = site.feed ? urlFor.call(hexo, site.feed.path) : '';
+    const email = (theme.profile && theme.profile.email) || site.email || "";
+    const feed = site.feed ? urlFor.call(hexo, site.feed.path) : "";
     const result = utils.parseConfig(configSchema, theme, {
       $email: email,
       $feed: feed,
-      $copyright: `&copy; ${new Date().getFullYear()} • <a href="${site.url}">${site.author}</a>`,
+      $copyright: `&copy; ${new Date().getFullYear()} • <a href="${site.url}">${
+        site.author
+      }</a>`,
       $gravatar: gravatar(email, 160),
       $title: site.title,
       $description: site.description,
@@ -62,9 +66,9 @@ module.exports = function (hexo) {
 
       result.sns = sns.reduce((ret, i) => {
         if (i.icon) {
-          if (i.icon === 'email') {
+          if (i.icon === "email") {
             i.url = `mailto:${i.url || email}`;
-          } else if (i.icon === 'feed') {
+          } else if (i.icon === "feed") {
             i.url = i.url || feed;
           }
           if (i.url) {
@@ -73,20 +77,26 @@ module.exports = function (hexo) {
         }
 
         if (i.template) {
-          ret.push([i.title || '', i.url || '', i.template]);
+          ret.push([i.title || "", i.url || "", i.template]);
         }
         return ret;
       }, []);
     }
 
     // theme vars
-    hexo.extend.injector.register('head_end', `<style is="theme">${css(utils.flattenObject(result.appearance))}</style>`);
+    hexo.extend.injector.register(
+      "head_end",
+      `<style is="theme">${css(utils.flattenObject(result.appearance))}</style>`
+    );
     // theme.js
-    hexo.extend.injector.register('head_end', `<script src="${urlFor.call(hexo, manifest.theme)}"></script>`);
+    hexo.extend.injector.register(
+      "head_end",
+      `<script src="${urlFor.call(hexo, manifest.theme)}"></script>`
+    );
 
     // disqus
     if (result.comments && result.comments.disqus) {
-      result.plugins.push(...execPlugin('disqus', result.comments.disqus));
+      result.plugins.push(...execPlugin("disqus", result.comments.disqus));
     }
 
     {
@@ -96,12 +106,17 @@ module.exports = function (hexo) {
         ...manifest.styles,
         ...manifest.scripts,
       ];
-      const injectorPoints = ['head_begin', 'head_end', 'body_begin', 'body_end'];
+      const injectorPoints = [
+        "head_begin",
+        "head_end",
+        "body_begin",
+        "body_end",
+      ];
       const positionedPlugins = { $t: [] };
 
       if (result.appearance.font && result.appearance.font.url) {
         entries.unshift({
-          position: 'head_end',
+          position: "head_end",
           template: `<link href="${result.appearance.font.url}" rel="stylesheet"></link>`,
         });
       }
@@ -115,18 +130,21 @@ module.exports = function (hexo) {
 
         if (!item) return;
 
-        if (typeof item === 'string') {
+        if (typeof item === "string") {
           // Direct with url
           if (!pluginManifest[item]) {
             const ext = path.extname(item);
             const src = urlFor.call(hexo, item);
-            if (ext === '.css') {
+            if (ext === ".css") {
               item = `<link href="${src}" rel="stylesheet">`;
-            } else if (ext === '.js') {
+            } else if (ext === ".js") {
               item = `<script src="${src}"></script>`;
             } else return process();
 
-            hexo.extend.injector.register(injectorPoints[ext === '.css' ? 1 : 3], item);
+            hexo.extend.injector.register(
+              injectorPoints[ext === ".css" ? 1 : 3],
+              item
+            );
             return process();
           }
 
@@ -140,7 +158,10 @@ module.exports = function (hexo) {
         }
 
         // Built in plugins
-        const executed = execPlugin(Object.keys(item)[0], Object.values(item)[0]);
+        const executed = execPlugin(
+          Object.keys(item)[0],
+          Object.values(item)[0]
+        );
         if (executed) {
           if (executed.length) entries.unshift(...executed);
           return process();
@@ -160,7 +181,10 @@ module.exports = function (hexo) {
         const index = positionedPlugins.$t.length;
         positionedPlugins.$t.push(utils.minifyHtml(loadSnippet(item.template)));
 
-        (Array.isArray(item.position) ? item.position : [item.position]).forEach((p) =>
+        (Array.isArray(item.position)
+          ? item.position
+          : [item.position]
+        ).forEach((p) =>
           (positionedPlugins[p] || (positionedPlugins[p] = [])).push(index)
         );
 
@@ -171,31 +195,40 @@ module.exports = function (hexo) {
     // override boolean value to html string
     if (result.footer.powered)
       result.footer.powered = __(
-        'footer.powered',
-        '<a href="https://hexo.io" target="_blank" rel="external nofollow noopener">Hexo</a>'
+        "footer.powered",
+        '<a href="https://maivietson.com" target="_blank" rel="external nofollow noopener">mVs</a>'
       );
     if (result.footer.theme)
       result.footer.theme =
-        __('footer.theme') +
-        ' - <a href="https://github.com/ikeq/hexo-theme-inside" target="_blank" rel="external nofollow noopener">Inside</a>';
+        __("footer.theme") +
+        ' - <a href="https://blog.maivietson.com/" target="_blank" rel="external nofollow noopener">Inside</a>';
 
     // root selector
-    hexo.extend.injector.register('body_begin', `<${manifest.root}></${manifest.root}>`);
+    hexo.extend.injector.register(
+      "body_begin",
+      `<${manifest.root}></${manifest.root}>`
+    );
 
     result.runtime = {
       styles: manifest.class,
       hash: utils.md5(
-        [...hexo.locals.getters.pages().sort('-date').toArray(), ...hexo.locals.getters.posts().sort('-date').toArray()]
+        [
+          ...hexo.locals.getters.pages().sort("-date").toArray(),
+          ...hexo.locals.getters.posts().sort("-date").toArray(),
+        ]
           .filter(utils.published)
           .map((i) => i.updated.toJSON())
-          .join('') +
+          .join("") +
           JSON.stringify(result) +
           pkg.version,
         6
       ),
 
       // runtime helpers
-      hasComments: !!(result.comments || (result.plugins && result.plugins.comments)),
+      hasComments: !!(
+        result.comments ||
+        (result.plugins && result.plugins.comments)
+      ),
       hasReward: !!result.reward,
       hasToc: !!result.toc,
       renderReadingTime: (() => {
@@ -204,7 +237,7 @@ module.exports = function (hexo) {
 
         let htmlToText = null;
         try {
-          htmlToText = require('html-to-text');
+          htmlToText = require("html-to-text");
         } catch {
           return false;
         }
@@ -212,7 +245,7 @@ module.exports = function (hexo) {
         const wpm = reading_time.wpm || 150;
         const compile = reading_time.text
           ? (o) => utils.sprintf(reading_time.text, o)
-          : (o) => __('post.reading_time', o);
+          : (o) => __("post.reading_time", o);
 
         return (content) => {
           const words = utils.countWord(
@@ -233,16 +266,19 @@ module.exports = function (hexo) {
       uriReplacer: (() => {
         let assetsFn = (src) => src;
         if (result.assets) {
-          const prefix = result.assets.prefix ? result.assets.prefix + '/' : '';
-          const suffix = result.assets.suffix || '';
-          assetsFn = (src) => prefix + `${src}${suffix}`.replace(/\/{2,}/g, '/');
+          const prefix = result.assets.prefix ? result.assets.prefix + "/" : "";
+          const suffix = result.assets.suffix || "";
+          assetsFn = (src) =>
+            prefix + `${src}${suffix}`.replace(/\/{2,}/g, "/");
         }
 
         return (src, assetPath) => {
-          assetPath = assetPath ? assetPath + '/' : '';
+          assetPath = assetPath ? assetPath + "/" : "";
 
           // skip both external and absolute path
-          return /^(\/\/?|http|data\:image)/.test(src) ? src : assetsFn(`${assetPath}${src}`);
+          return /^(\/\/?|http|data\:image)/.test(src)
+            ? src
+            : assetsFn(`${assetPath}${src}`);
         };
       })(),
     };
@@ -262,13 +298,13 @@ module.exports = function (hexo) {
         md5: utils.md5,
         i18n: (template) =>
           template.replace(/{{([\.a-zA-Z0-9_\| ]+)}}/g, (_, $1) => {
-            const [key, t] = $1.split('|').map((i) => i.trim());
-            const payload = t.split(',');
+            const [key, t] = $1.split("|").map((i) => i.trim());
+            const payload = t.split(",");
             return __(
               key,
-              t.includes(':')
+              t.includes(":")
                 ? payload.reduce((map, i) => {
-                    const [k, v] = i.split(':');
+                    const [k, v] = i.split(":");
                     return {
                       ...map,
                       [k.trim()]: v.trim() || true,
@@ -295,7 +331,7 @@ module.exports = function (hexo) {
     const templateUrl = path.join(hexo.base_dir, pathOrCode);
     if (!fs.existsSync(templateUrl)) return pathOrCode;
 
-    return fs.readFileSync(templateUrl, 'utf8');
+    return fs.readFileSync(templateUrl, "utf8");
   }
 };
 
@@ -305,7 +341,7 @@ function loadManifest(includePaths = []) {
   function load(dir) {
     let map = {};
     try {
-      const manifestPath = path.join(dir, 'manifest.json');
+      const manifestPath = path.join(dir, "manifest.json");
       if (fs.existsSync(manifestPath)) {
         map = require(manifestPath) || {};
       }
